f9361109cccf6e67f763420b346e88c6dd1d4e06
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-09-28 10:28:46 +0800

    refactor: add drink to order list

--- index.js
+++ index.js
@@ -27,40 +27,94 @@ Drink.prototype.price = function () {
 // console.log(bubbleMilkTea)
 // console.log(bubbleMilkTea.price())
 
-let drinkName = 'Black Tea'
-let drinkIce = 'Regular Ice'
-let drinkSugar = 'Regular Sugar'
-const orderList = [] 
+// let drinkName = 'Black Tea'
+// let drinkIce = 'Regular Ice'
+// let drinkSugar = 'Regular Sugar'
+// const orderList = []
 
-const drinks = document.querySelectorAll("input[name='drink']")
-const ice = document.querySelectorAll("input[name='ice']")
-const sugar = document.querySelectorAll("input[name='sugar']")
-const addBtn = document.querySelector('.add')
-let order_list = document.querySelector('.order-list')
+// const drinks = document.querySelectorAll("input[name='drink']")
+// const ice = document.querySelectorAll("input[name='ice']")
+// const sugar = document.querySelectorAll("input[name='sugar']")
+const addBtn = document.querySelector('[data-alpha-pos="add-drink"]')
 
-drinks.forEach(d => {
-  d.addEventListener('click', () => {
-    drinkName = d.value
-  })
-})
+let orderLists = document.querySelector('[data-order-list]')
 
-ice.forEach(i => {
-  i.addEventListener('change', () => {
-    drinkIce = i.value
-  })
-})
+// drinks.forEach(d => {
+//   d.addEventListener('click', () => {
+//     drinkName = d.value
+//   })
+// })
 
-sugar.forEach(s => {
-  s.addEventListener('change', () => {
-    drinkSugar = s.value
-  })
-})
+// ice.forEach(i => {
+//   i.addEventListener('change', () => {
+//     drinkIce = i.value
+//   })
+// })
+
+// sugar.forEach(s => {
+//   s.addEventListener('change', () => {
+//     drinkSugar = s.value
+//   })
+// })
 
 addBtn.addEventListener('click', () => {
-  orderList.push(new Drink(drinkName, drinkIce, drinkSugar))
-  console.log(orderList)
-  renderOrderList()
+  // orderList.push(new Drink(drinkName, drinkIce, drinkSugar))
+  // console.log(orderList)
+  // renderOrderList()
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const drinkIce = alphaPos.getCheckedValue('ice')
+  const drinkSugar = alphaPos.getCheckedValue('sugar')
+
+  if (!drinkName) {
+    alert('Please choose at least one item.')
+    return
+  }
+
+  const drink = new Drink(drinkName, drinkIce, drinkSugar)
+  console.log(drink)
+  console.log(drink.price())
+
+  alphaPos.addDrink(drink)
 })
+
+function AlphaPos () {}
+
+AlphaPos.prototype.getCheckedValue = (inputName) => {
+  let selectedOption = ''
+  document.querySelectorAll(`input[name='${inputName}']`).forEach( (item) => {
+    if (item.checked) {
+      selectedOption = item.value
+    }
+  })
+  return selectedOption
+}
+
+AlphaPos.prototype.addDrink = (drink) => {
+  const orderListsCard = `
+  <div class="card mb-3" style="width: 100%;">
+    <div class="card-body">
+      <div class="text-end">
+        <button type="button" class="btn-close" aria-label="Close" data-alpha-pos="delete-drink"></button>
+      </div>
+      <div>
+        ${drink.name}
+      </div>
+      <div>
+        ${drink.ice}
+      </div>
+      <div>
+        ${drink.sugar}
+      </div>
+    </div>
+    <div class="card-footer text-end text-muted">
+      $ <span data-drink-price>${drink.price()}</span>
+    </div>
+  </div>
+  `
+  orderLists.insertAdjacentHTML('afterbegin', orderListsCard)
+}
+
+const alphaPos = new AlphaPos()
 
 function renderOrderList() {
   console.log('render')
